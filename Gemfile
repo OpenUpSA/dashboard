132c0fda4e35cd04d4abac133215b313340b546d
Greg Kempe <greg@kempe.net>
2015-02-22 13:02:10 +0200

    Improve logging and error handling of GA

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ ruby '2.1.1'
 gem 'dashing'
 gem 'hashie'
 
+gem 'log4r'
 gem 'google-api-client'
 gem 'mailchimp-api', '~> 2.0.5'
--- jobs/google_analytics.rb
+++ jobs/google_analytics.rb
@@ -1,4 +1,9 @@
 require 'google/api_client'
+require 'log4r'
+
+logger = Log4r::Logger.new('ga')
+logger.outputters << Log4r::StderrOutputter.new('stderr')
+logger.outputters[0].formatter = Log4r::SimpleFormatter.new
 
 google_account_id = '301981928719-8l5uovftgnct3fqltt6r8k2ut25o4tp8@developer.gserviceaccount.com'
 
@@ -11,8 +16,10 @@ client.authorization = Signet::OAuth2::Client.new(
   :scope => 'https://www.googleapis.com/auth/analytics',
   :issuer => google_account_id,
   :signing_key => key)
+logger.info "Setting up Google Analytics Client"
 client.authorization.fetch_access_token!
 ga = client.discovered_api('analytics', 'v3') 
+logger.info "Client ready"
 
 graphs = {}
 graph_data = {}
@@ -25,10 +32,13 @@ for website in get_websites
 end
 
 # walk through accounts and match tracking IDs to property IDs
+logger.info "Mapping tracking IDs to property IDs"
 for account in client.execute(api_method: ga.management.accounts.list).data.items
   response = client.execute(api_method: ga.management.webproperties.list, parameters: {accountId: account.id})
 
-  if response.data
+  if response.error?
+    logger.error response.error_message
+  elsif response.data
     graphs.each_pair do |graph_id, tracking_id|
       for property in response.data.items
         if property.id == tracking_id
@@ -54,6 +64,8 @@ SCHEDULER.every '1h', first_in: 0 do
   start_date = now - 7 * 3600 * 24
 
   graphs.each_pair do |graph_id, property_id|
+    logger.info "Getting historical data for #{graph_id} (property #{property_id})"
+
     response = client.execute(api_method: ga.data.ga.get, parameters: {
       ids: "ga:#{property_id}",
       'start-date' => start_date.strftime('%Y-%m-%d'),
@@ -62,16 +74,22 @@ SCHEDULER.every '1h', first_in: 0 do
       dimensions: 'ga:date',
     })
 
-    if response.data?
+    if response.error?
+      logger.error "#{graph_id}: #{response.error_message}"
+    elsif response.data?
+      logger.info "#{graph_id}: Got #{response.data.rows.length} historical rows"
+
       data = graph_data[graph_id]
-      data['historical'] = []
+      historical = []
 
       response.data.rows.each_with_index do |row, i|
-        data['historical'] << {
+        historical << {
           x: Time.parse(row[0]).to_i,
           y: row[1].to_i
         }
       end
+
+      data['historical'] = historical
     end
   end
 end
@@ -85,7 +103,9 @@ SCHEDULER.every '30s', first_in: 0 do
       metrics: 'rt:activeUsers'
     })
 
-    if response.data?
+    if response.error?
+      logger.error "#{graph_id}: realtime data error: #{response.error_message}"
+    elsif response.data?
       data = graph_data[graph_id]
 
       if response.data.totalResults > 0
